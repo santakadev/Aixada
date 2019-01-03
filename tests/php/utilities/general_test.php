99bb9a037967ba9ced817a351ebf17db958af1c1
Daniel Santamaría <santaka87@gmail.com>
2019-01-03 12:31:19 +0100

    refactor do_stored_query

--- tests/php/utilities/general_test.php
+++ tests/php/utilities/general_test.php
@@ -8,13 +8,39 @@ require_once(__DIR__.'/DBWrapTestingDoStoredQuery.php');
 
 final class general_test extends \PHPUnit\Framework\TestCase
 {
-    public function test_do_stored_query()
+    /**
+     * @dataProvider do_stored_query_data_provider
+     * @param $arguments
+     * @param $executedQuery
+     */
+    public function test_do_stored_query($arguments, $executedQuery)
     {
         $testingDb = new DBWrapTestingDoStoredQuery();
 
         DBWrap::TEST_set_instance($testingDb);
-        do_stored_query('get_users');
+        do_stored_query($arguments);
 
-        $this->assertSame('CALL get_users()', $testingDb->executedQuery());
+        $this->assertSame($executedQuery, $testingDb->executedQuery());
+    }
+
+    public function do_stored_query_data_provider()
+    {
+        return [
+            [
+                ['get_users'],
+                'CALL get_users()'
+            ],
+            [
+                [
+                    'get_orders_listing',
+                    '2018-11-03',
+                    '9999-12-30',
+                    1,
+                    0,
+                    10
+                ],
+                "CALL get_orders_listing('2018-11-03','9999-12-30','1','0','10')"
+            ]
+        ];
     }
 }
